f3096cff8d650e0d61772b963d0a93d410fe1b22
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-06-02 21:35:52 +0530

    First vid practice done:

---
+++ first.js
@@ -0,0 +1,17 @@
+const product = {
+    title: "ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+
+console.log(product);
+
+const profile ={
+    username: "ShradhaKhapra",
+    followers: 123,
+    following: 4,
+    isFollow: false
+};
+
+console.log(profile);
