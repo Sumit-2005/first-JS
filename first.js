896ef6618769eb5b072c6fc92cd5a3ed4ba1b43b
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-06-03 21:38:22 +0530

    second exercise completed

--- first.js
+++ first.js
@@ -19,11 +19,26 @@ console.log(profile);
 
 
 //Second Exercise
-let num =prompt("Enter a Number:")
+// let num =prompt("Enter a Number:")
 
-if(num%5 === 0){
-    console.log(num, "is a multiple of 5");
+// if(num%5 === 0){
+//     console.log(num, "is a multiple of 5");
+// } else {
+//     console.log(num, "is not a multiple of 5");
+// }
+
+let marks = prompt("Enter your marks (0-100):")
+let grade;
+if(marks>=90 && marks<=100) {
+    grade = "A";
+} else if(marks>=70 && marks<=89) {
+    grade = "B";
+} else if(marks>=60 && marks<=69) {
+    grade = "C";
+} else if(marks>=50 && marks<=59) {
+    grade = "D";
 } else {
-    console.log(num, "is not a multiple of 5");
+    grade = "F";
 }
 
+console.log(grade);
