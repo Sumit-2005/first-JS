8fd4209ddec2476b8351ff3795a3705fbe048d27
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-06-07 16:23:36 +0530

    fifth vid exercise done

--- Second.js
+++ Second.js
@@ -32,9 +32,38 @@ let countv = (msg) => {
 
 
 
- let arr = [1,2,3,4,5,6]
+//  let arr = [1,2,3,4,5,6]
 
- arr.forEach((val) => {
-    console.log(val**2);
- });
+//  arr.forEach((val) => {
+//     console.log(val**2);
+//  });
 
+
+// let arr = [87,93,42,99,98,92,42,54,66,71];
+
+// let toppr = arr.filter((val) => {
+//     return val>=90;
+// });
+
+// console.log(toppr);
+
+
+
+let n = prompt("Enter a number :");
+let arr =[]
+
+for(i=1; i<=n; i++) {
+    arr[i-1] = i;
+}
+console.log(arr);
+
+
+let sum = arr.reduce((prev, curr) => {
+    return prev+curr;
+});
+console.log(sum);
+
+let prod =arr.reduce((prev , curr) => {
+    return prev*curr;
+})
+console.log(prod)
