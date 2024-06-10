e2df0a39cfbabf111850345978e60b3a76307b4d
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-06-10 16:09:55 +0530

    Seventh vid exercise done

---
+++ seventh.js
@@ -0,0 +1,10 @@
+let btn = document.createElement("button");
+btn.innerText = "Click Me!";
+btn.style.color = "white";
+btn.style.backgroundColor= "red";
+document.querySelector("body").prepend(btn);
+
+
+let p = document.querySelector("p");
+p = p.classList.add("newClass");
+
