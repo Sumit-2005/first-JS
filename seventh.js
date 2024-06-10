0276dc0de729b2564ce40e73297c01c4a835f996
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-06-10 16:10:59 +0530

    Space elimination and comments addition

--- seventh.js
+++ seventh.js
@@ -1,10 +1,10 @@
+// Q1
 let btn = document.createElement("button");
 btn.innerText = "Click Me!";
 btn.style.color = "white";
 btn.style.backgroundColor= "red";
 document.querySelector("body").prepend(btn);
 
-
+// Q2
 let p = document.querySelector("p");
 p = p.classList.add("newClass");
-
