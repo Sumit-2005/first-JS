d418e3b5d05950fd7a63d9b34cee8bd8d89450bf
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-06-12 00:54:09 +0530

    eight vid exercise not solved : Unknown error

---
+++ eight.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector("#change");
+let mode = "light";
+
+btn.addEventListener("click", () => {
+    if (mode === "light") {
+        mode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    } else {
+        mode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+
+    console.log(mode);
+});
